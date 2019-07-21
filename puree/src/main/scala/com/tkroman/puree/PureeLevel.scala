35947e82285a9e86045b53990e30074e3247249e
Roman Tkalenko <rmn.tk.ml@gmail.com>
2019-07-21 22:07:19 +0300

    remove unused code

--- puree/src/main/scala/com/tkroman/puree/PureeLevel.scala
+++ puree/src/main/scala/com/tkroman/puree/PureeLevel.scala
@@ -1,12 +1,8 @@
 package com.tkroman.puree
 
-sealed trait PureeLevel {
-  def isOff: Boolean = false
-}
+sealed trait PureeLevel
 object PureeLevel {
-  case object Off extends PureeLevel {
-    override def isOff: Boolean = true
-  }
+  case object Off extends PureeLevel
   case object Effect extends PureeLevel
   case object Strict extends PureeLevel
 }
